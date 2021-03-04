1c458aaec856a4b632f7d481c926339ba664d2b7
ngwoon <todory2002@gmail.com>
2021-03-04 23:53:54 +0900

    Remove logging for debug

--- nodebird/passport/kakaoStrategy.js
+++ nodebird/passport/kakaoStrategy.js
@@ -8,7 +8,6 @@ module.exports = () => {
         clientID: process.env.KAKAO_ID,
         callbackURL: "/auth/kakao/callback",
     }, async (accessToken, refreshToken, profile, done) => {
-        console.log("kakao profile", profile);
         try {
             const exUser = await User.findOne({ where: { snsId: profile.id, provider: "kakao" }});
             if(exUser) {
